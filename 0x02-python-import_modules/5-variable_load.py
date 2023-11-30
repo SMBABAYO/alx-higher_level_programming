e28fcf29c98d684d12db36660811c9778fef25c5
SMBABAYO <smbabayo@gmail.com>
2023-11-30 13:50:06 -0800

    variable

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-from variable_load import a
-
-def find_var():
-    print(a)
-
-if _name__ == "__main__":
-    find_var)
+from variable_load_5 import a
+if __name__ == "__main__":
+    print("{:d}".format(a))
