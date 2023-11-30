e0caf74d39c37d9cf266b7889bd3e39f419d7ba8
SMBABAYO <smbabayo@gmail.com>
2023-11-30 12:46:33 -0800

    infinite

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    length = len(sys.argv)
+    total = 0
+    if length == 1:
+        total = 0
+    else:
+        for x in range(1, length):
+            total += int(sys.argv[x])
+    print("{:d}".format(total))
