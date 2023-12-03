99db222f8be72663c8314696416a010c5a6b50cd
SMBABAYO <smbabayo@gmail.com>
2023-12-03 10:43:49 -0800

    python

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -2,7 +2,8 @@
 #include <stdio.h>
 #include <Python.h>
 /**
- * print_python_list_info -  function that prints some info
+ * print_python_list_info -  function that prints some basic
+ *							info about Python lists
  * @p: python list
  */
 void print_python_list_info(PyObject *p)
